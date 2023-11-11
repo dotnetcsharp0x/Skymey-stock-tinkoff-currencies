1e9f365903186735b1ea7830255a26617ab73353
dotnetcsharp0x <dsotnikov1996@gmail.com>
2023-11-11 08:49:11 +0300

    Добавьте файлы проекта.

---
+++ Skymey-stock-tinkoff-currencies/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Skymey_stock_tinkoff_currencies
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
